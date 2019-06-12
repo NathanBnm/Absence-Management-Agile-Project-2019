e3c73a21c48939147e5eadac76219e4d1b1bea7d
Sakura_Isana <sakura.troyard@gmail.com>
2019-06-12 15:52:38 +0200

    Fonction
    Suppression des absences dans la table avec la fonction delete_absences

--- functions/login.func.php
+++ functions/login.func.php
@@ -27,27 +27,66 @@
         return $user_rank;
     }
 
-    /*
-    function grab_absence($username){
+    //les enseignants ou le directeur des études peuvent y accéder, pas les étudiants
+    function saisie_absence($username, $usernametu, $codecours, $commentaire, $motif, $type, $date){
         global $db;
         $u = [
-            'UTI_IDENTIFIANT' => $username
+            'UTI_IDENTIFIANT_ENSEIGNANT'    =>  $username,
+            'UTI_IDENTIFIANT_ELEVE'         =>  $usernametu,
+            'COU_CODE'                      =>  $codecours,
+            'SIG_COMMENTAIRE'               =>  $commentaire,
+            'SIG_MOTIF'                     =>  $motif,
+            'SIG_TYPE'                      =>  $type,
+            'SIG_DATE'                      =>  $date
+
         ];
-        $sql = "";
-        $req->execute($u);
-        return $req;
+        $sql = "INSERT INTO ABS_SIGNALEMENT (UTI_CODE, UTI_CODE_1, COU_CODE, SIG_COMMENTAIRE, SIG_MOTIF, SIG_TYPE, SIG_DATE) VALUES (:UTI_IDENTIFIANT_ENSEIGNANT, :UTI_IDENTIFIANT_ELEVE, :COU_CODE, :SIG_COMMENTAIRE, :SIG_MOTIF, :SIG_TYPE, :SIG_DATE)";
+        $req = $db->prepare($sql);
+        $req->execute($sql);
     }
 
-    function delete_absence($usernamens, $usernametu, $date){
-            /*UTI_CODE => personne qui a signalé l'absence seule qui peut supprimer*//*
+    function origine_absence($username, $usernametu){
         global $db;
         $u = [
-            'UTI_IDENTIFIANT_ENSEIGNANT' => $usernamens;
-            'UTI_IDENTIFIANT_ELEVE' => $usernametu;
+            'UTI_IDENTIFIANT_ENSEIGNANT' => $usernamens,
+            'UTI_IDENTIFIANT_ELEVE'      => $usernametu
         ];
         $sql = "SELECT * from ABS_SIGNALEMENT WHERE UTI_CODE = ':UTI_ENSEIGNANT' AND UTI_CODE_1 = 'UTI_IDENTIFIANT_ELEVE' ";
+        $req = $db->prepare($sql);
+        $ens_eleve = [];
+        while($row = $req->fetchObject()){
+            $ens_eleve['absence'] = $row;
+        }
+        return $ens_eleve;
         
     }
-    */
+    
+    //demander vérification à l'utilisateur
+    function delete_absence($username, $usernametu, $date){
+        global $db;
+        $u = [
+            'UTI_IDENTIFIANT_ENSEIGNANT' => $usernamens,
+            'UTI_IDENTIFIANT_ELEVE'      => $usernametu,
+            'SIG_DATE'                   => $date
+        ];
+        $absence[];
+        $absence[] = origine_absence($username,$usernametu)['absence']->DATE;
+        foreach($absence as $row){
+            if($absence['row'] == $date){
+                $sql = "DELETE from ABS_SIGNALEMENT WHERE UTI_CODE = ':UTI_IDENTIFIANT_ENSEIGNANT' AND UTI_CODE_1 = ':UTI_IDENTIFIANT_ELEVE' AND SIG_DATE = ':DATE'";
+                $req = $db->prepare($sql);
+                $req->execute($sql);
+                echo "L'absence a bien été supprimée";
+            }
+            else{
+                echo "Pas d'absence à supprimer";
+            }
+        }
+           
+        
+    }
+
+    
+    
    
 ?>
