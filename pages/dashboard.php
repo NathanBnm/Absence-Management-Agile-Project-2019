4730515363d8352e1d874f9bf3bb6c3861e8920b
Nathan Bonnemains <bonnemainsnathan@gmail.com>
2019-06-12 17:01:05 +0200

    Couleurs

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -1,99 +1,105 @@
 <?php
-    if (isset($_SESSION['rank']) && $_SESSION['rank'] == 'DIR') {
-?>
-    <style>
-    #fixe-haut
-{
-    background      : #8CB3C3 ;
-    height          : 70px;
-    position        : fixed;
-    top             : 0;
-    width           : 100%;
-    left            : 0;
-}
-.submenu{
-	display: none;
-}
-nav > ul{
-	margin:0px
-	padding: 0px;
-}
-
-nav > ul >li{
-	float: left;
-}
-nav{
-	width:100%;
-	background-color: #8CB3C3;
-}
-nav > ul::after{
-	content:"";
-	display: block;
-	clear: both;
-}
-nav a{
-	display: infinite-block;
-	text-decoration: none;
-}
-nav > ul > li > a{
-	padding: 20px 30px;
-	color: #FFF;
-}
-nav li:hover .submenu{
-	display: inline-block;
-	position: absolute;
-	top:100%;
-	left:0%;
-	padding:0%;
-	z-index:100000;
-}
-.submenu li{
-	border-bottom : 1px solid #CCC;
-}
-.submenu li a{
-	padding : 15px;
-	font-size: 13px;
-	color:#222538;
-	width: 270px;
-}
-.menu-htlm:hover{
-	border-top: 5px solid #e44d26;
-	background-color: RGBa(60, 172, 196, 0.15);
-}
-nav > ul > li:hover a{
-	padding: 15px 30px 20px 30px;
-}
-.menu-html .submenu{
-	background-color: RGB(124,170,189);
-}
-.menu-html .submenu li:hover{
-	background-color: RGB(158 ,187,198);
-}
-h1 {
-	color: #0092B3;
-	text-align: center;
-	font-size: 70px;
-}
-p {
-	text-align: center;
-  }</style>
-    <div id="fixe-haut">
-        <img src="../img/DECO.png" alt="Deco" title="Yes" height="70" width="70" align="right" />
-        <nav>
-            <ul>
-                <li class="menu-html"><a href=html.html">MENU</a>
-                    <ul class="submenu">
-                        <li><a href="#">CHOIX 1</a></li>
-                        <li><a href="#">CHOIX 2</a></li>
-                    </ul>
-                </li>
-            </ul>
-            <nav>
-    </div>
-    </br></br></br></br></br>
-    <h1>Connexion Directeur Etude :</h1></br></br></br></br></br></br>
-    <p><img src="../img/logoiut.png" alt="logo" title="Unicaen" height="300" width="570"/ align"center"></br></p>
+if (isset($_SESSION['rank']) && $_SESSION['rank'] == 'ETU') {
+	?>
+	<nav class="navbar bleu" role="navigation" aria-label="main navigation">
+		<div id="navbar" class="navbar-menu">
+			<div class="navbar-start">
+				<div class="navbar-item has-dropdown is-hoverable">
+					<a class="navbar-link has-dropdown">
+						<i class="fas fa-portrait" style="font-size: 30px; padding: 10%"></i>
+						Etudiant
+					</a>
+					<div class="navbar-dropdown is-left">
+						<a class="navbar-item">
+							Mes Informations
+						</a>
+					</div>
+				</div>
+				<a class="navbar-item">
+					Accueil
+				</a>
+				<a class="navbar-item">
+					Absences
+				</a>
+				<a class="navbar-item">
+					Retards
+				</a>
+			</div>
+			<div class="navbar-end">
+				<a class="navbar-item">
+					<i class="fas fa-power-off" style="font-size: 30px"></i>
+				</a>
+			</div>
+		</div>
+	</nav>
+<?php
+} else if (isset($_SESSION['rank']) && $_SESSION['rank'] == 'ENS') {
+	?>
+	<nav class="navbar orange" role="navigation" aria-label="main navigation">
+		<div id="navbar" class="navbar-menu">
+			<div class="navbar-start">
+				<div class="navbar-item has-dropdown is-hoverable">
+					<a class="navbar-link has-dropdown">
+						<i class="fas fa-portrait" style="font-size: 30px; padding: 10%"></i>
+						Enseignant
+					</a>
+					<div class="navbar-dropdown is-left">
+						<a class="navbar-item">
+							Mes Informations
+						</a>
+					</div>
+				</div>
+				<a class="navbar-item">
+					Accueil
+				</a>
+				<a class="navbar-item">
+					Absences
+				</a>
+				<a class="navbar-item">
+					Retards
+				</a>
+			</div>
+			<div class="navbar-end">
+				<a class="navbar-item">
+					<i class="fas fa-power-off" style="font-size: 30px"></i>
+				</a>
+			</div>
+		</div>
+	</nav>
+<?php
+} else if (isset($_SESSION['rank']) && $_SESSION['rank'] == 'DIR') {
+	?>
+	<nav class="navbar violet" role="navigation" aria-label="main navigation">
+		<div id="navbar" class="navbar-menu">
+			<div class="navbar-start">
+				<div class="navbar-item has-dropdown is-hoverable">
+					<a class="navbar-link has-dropdown">
+						<i class="fas fa-portrait" style="font-size: 30px; padding: 10%"></i>
+						Directeur
+					</a>
+					<div class="navbar-dropdown is-left">
+						<a class="navbar-item">
+							Mes Informations
+						</a>
+					</div>
+				</div>
+				<a class="navbar-item">
+					Accueil
+				</a>
+				<a class="navbar-item">
+					Absences
+				</a>
+				<a class="navbar-item">
+					Retards
+				</a>
+			</div>
+			<div class="navbar-end">
+				<a class="navbar-item">
+					<i class="fas fa-power-off" style="font-size: 30px"></i>
+				</a>
+			</div>
+		</div>
+	</nav>
 <?php
 }
-
 ?>
