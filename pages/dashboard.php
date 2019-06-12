fb381c240f8a3270e38a04b25d3e33823d670e42
Nathan Bonnemains <bonnemainsnathan@gmail.com>
2019-06-12 15:06:36 +0200

    Test

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -1,8 +1,7 @@
 <h1>Tableau de bord</h1>
 <?php
-    $username = 21801526;
+    $username = "21504680";
     $var = user_rank($username)['rank']->CAT_CODE;
     echo $var;
-
 ?>
 <p>Si aucun message d'erreur s'affiche c'est que la connexion à la base de données n'a pas echoué</p>
