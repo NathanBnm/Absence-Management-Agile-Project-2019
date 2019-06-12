0a3065162e1f2d0f72f6c0a27d977c03f916f6a0
Nathan Bonnemains <bonnemainsnathan@gmail.com>
2019-06-12 10:43:17 +0200

    Identifiants BDD

--- functions/main-functions.php
+++ functions/main-functions.php
@@ -7,9 +7,9 @@ session_start();
 
 //DB
 $dbhost = 'localhost';
-$dbname = 'animalia';
-$dbuser = 'root';
-$dbpswd = '';
+$dbname = 'agile3_bd';
+$dbuser = 'agile3';
+$dbpswd = 'Viwaik7ahcoo8che';
 
 try {
     $db = new PDO('mysql:host=' . $dbhost . ';dbname=' . $dbname, $dbuser, $dbpswd, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8', PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
