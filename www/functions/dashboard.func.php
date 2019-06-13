cfeaa3d0cd5262724d5b1b2ea18f6d2fd0a4875e
Baptiste_Moreau <Baptiste_Moreau@LAPTOP-DBBRBAUD>
2019-06-13 22:58:17 +0200

    Ajout de l'affichage des derniers billets

--- www/functions/dashboard.func.php
+++ www/functions/dashboard.func.php
@@ -52,3 +52,26 @@ function recuperer_mail() {
     }
     return $mails;
 }
+
+function last_ticket(){
+    global $db;
+    $u = [
+        'UTI_IDENTIFIANT' => $_SESSION['id']
+    ];
+    $sql = "SELECT absence.SIG_COMMENTAIRE, absence.SIG_MOTIF, absence.SIG_ETAT, etu.UTI_PRENOM, etu.UTI_NOM, etu.UTI_IDENTIFIANT, cours.COU_MODULE, SIG_DATE, SIG_TRAITE, COU_TYPE, SIG_ETAT, SIG_TYPE
+            FROM ABS_BILLET absence
+            JOIN ABS_COURS cours ON absence.COU_CODE = cours.COU_CODE
+            JOIN ABS_UTILISATEUR etu ON etu.UTI_CODE = absence.UTI_CODE_1
+            AND absence.UTI_CODE = (SELECT UTI_CODE FROM ABS_UTILISATEUR WHERE UTI_IDENTIFIANT = :UTI_IDENTIFIANT)
+            ORDER BY SIG_CODE DESC
+            LIMIT 2";
+    $req = $db->prepare($sql);
+    $req->execute($u);
+    $user_last_ticket = [];
+    $i = 0;
+    while($row = $req->fetchObject()){
+        $user_last_ticket[$i] = $row;
+        $i++;
+    }
+    return $user_last_ticket;
+}
--- www/includes/teacher/dashboard.php
+++ www/includes/teacher/dashboard.php
@@ -30,75 +30,80 @@
                     <p>Vos derniers billets</p>
                 </div>
                 <div class="message-body">
-                    <div class="card" style="margin-bottom: 5%;">
-                        <header class="card-header">
-                            <p class="card-header-title">
-                                <span class="tag is-warning is-rounded" style="margin-right: 10px;">Retard</span>
-                                <span class="tag is-danger is-rounded" style="margin-right: 10px;">Non Justifié</span>
-                                Nom Prénom | Promo - Groupe
-                            </p>
-                        </header>
-                        <div class="card-content">
-                            <div class="content">
-                                <span class="tag is-success is-rounded" style="margin-right: 10px;">M2101</span>
-                                <span class="tag is-info is-rounded" style="margin-right: 10px;">16 Juin 2019 - 11h39</span> <br>
-                                <strong>Commentaire:<br></strong>
-                                En Retard de 30 minutes.
-                            </div>
-                            <div class="columns" style="text-align: center">
-                                <div class="column">
-                                    <a href="#" class="button is-link" aria-haspopup="true" onclick="m1.m1_acc.open()">
-                                        <span class="icon is-small">
-                                            <i class="far fa-edit"></i>
-                                        </span>
-                                        <span>Modifier</span>
-                                    </a>
-                                </div>
-                                <div class="column">
-                                    <a class="button is-danger">
-                                        <span class="icon is-small">
-                                            <i class="fas fa-trash-alt"></i>
-                                        </span>
-                                        <span>Supprimer</span>
-                                    </a>
+                    <div class="tile is-parent is-vertical">
+                        <?php
+                            $tickets = last_ticket();
+                            foreach($tickets as $ticket) {
+                        ?>
+                        <div class="tile is-child">
+                            <div class="card" style="margin-bottom: 1%;">
+                                <header class="card-header">
+                                    <p class="card-header-title">
+                                            <span class="tags has-addons"  style="margin-right: 10px;">
+                                                <span class="tag is-warning is-rounded">
+                                                    <?php
+                                                    if(strtoupper($ticket->SIG_TYPE) == "A") {
+                                                        echo "Absence";
+                                                    } else {
+                                                        echo "Retard";
+                                                    }
+                                                    ?>
+                                                </span>
+                                                <span class="tag is-danger is-rounded"><?php echo $ticket->SIG_ETAT; ?></span>
+                                                <span class="tag is-success is-rounded">
+                                                    <?php
+                                                    if($ticket->SIG_TRAITE == 0) {
+                                                        echo "Non traité";
+                                                    } else {
+                                                        echo "Traité";
+                                                    }
+                                                    ?>
+                                                </span>
+                                            </span>
+                                        <?php echo $ticket->UTI_PRENOM; ?> <?php echo $ticket->UTI_NOM; ?> (<?php echo $ticket->UTI_IDENTIFIANT; ?>)
+                                    </p>
+                                </header>
+                                <div class="card-content">
+                                    <div class="content">
+                                        <div class="tile is-parent">
+                                            <div class="tile is-child">
+                                                <strong>Module : </strong><span class="tag is-success is-rounded" style="margin-left: 10px;"><?php echo $ticket->COU_MODULE; ?></span> <br>
+                                                <strong>Date :</strong><span class="tag is-info is-rounded" style="margin-left: 10px;"><?php echo $ticket->SIG_DATE; ?></span> <br>
+                                                <strong>Type de Cours : </strong><span class="tag is-info is-rounded" style="margin-left: 10px;"><?php echo strtoupper($ticket->COU_TYPE); ?></span><br>
+                                                <strong>Motif : </strong><?php echo $ticket->SIG_MOTIF; ?><br>
+                                            </div>
+                                            <div class="tile is-child">
+                                                <strong>Commentaire:<br></strong>
+                                                <div class="control">
+                                                    <textarea class="textarea has-fixed-size"  rows="3" readonly><?php echo $ticket->SIG_COMMENTAIRE; ?></textarea>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="columns" style="text-align: center">
+                                        <div class="column">
+                                            <a href="#" class="button is-link" aria-haspopup="true" onclick="m1.m1_acc.open()">
+                                                <span>Modifier</span>
+                                                <span class="icon is-small">
+                                                        <i class="far fa-edit"></i>
+                                                    </span>
+                                            </a>
+                                        </div>
+                                        <div class="column">
+                                            <a class="button is-danger">
+                                                <span>Supprimer</span>
+                                                <span class="icon is-small">
+                                                        <i class="fas fa-trash-alt"></i>
+                                                    </span>
+                                            </a>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="card">
-                        <header class="card-header">
-                            <p class="card-header-title">
-                                <span class="tag is-warning is-rounded" style="margin-right: 10px;">Retard</span>
-                                <span class="tag is-danger is-rounded" style="margin-right: 10px;">Non Justifié</span>
-                                Nom Prénom | Promo - Groupe
-                            </p>
-                        </header>
-                        <div class="card-content">
-                            <div class="content">
-                                <span class="tag is-success is-rounded" style="margin-right: 10px;">M2101</span>
-                                <span class="tag is-info is-rounded" style="margin-right: 10px;">16 Juin 2019 - 11h39</span> <br>
-                                <strong>Commentaire:<br></strong>
-                                En Retard de 30 minutes.
-                            </div>
-                            <div class="columns" style="text-align: center">
-                                <div class="column">
-                                    <a href="#" class="button is-link" aria-haspopup="true" onclick="m1.m1_acc.open()">
-                                        <span class="icon is-small">
-                                            <i class="far fa-edit"></i>
-                                        </span>
-                                        <span>Modifier</span>
-                                    </a>
-                                </div>
-                                <div class="column">
-                                    <a class="button is-danger">
-                                        <span class="icon is-small">
-                                            <i class="fas fa-trash-alt"></i>
-                                        </span>
-                                        <span>Supprimer</span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
+                                <?php
+                            }
+                        ?>
                     </div>
                 </div>
             </article>
