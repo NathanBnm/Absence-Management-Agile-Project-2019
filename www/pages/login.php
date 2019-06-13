c9c0d391e54ed8bccb67df6a71206709c5276d0d
NathanC <21716210@etu.unicaen.fr>
2019-06-13 16:16:21 +0200

    notification fonctionnelles + MY BIG BURGER

--- www/pages/login.php
+++ www/pages/login.php
@@ -1,5 +1,46 @@
 <?php
 
+function envoie($mail){
+
+    if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail))
+    {
+        $passage_ligne = "\r\n";
+    }
+    else
+    {
+        $passage_ligne = "\n";
+    }
+
+
+    $message_txt = "Salut à tous, voici un e-mail envoyé par un script PHP.";
+    $message_html = "<html><head></head><body><b>Salut à tous</b>, voici un e-mail envoyé par un <i>script PHP</i>.</body></html>";
+
+    $boundary = "-----=" . md5(rand());
+
+    $sujet = "Absence";
+
+    $header = "From: \"EXPEDITEUR\"noreply@unicaen.fr".$passage_ligne;
+    $header.= "MIME-Version: 1.0".$passage_ligne;
+    $header.= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
+
+    $message = $passage_ligne."--".$boundary.$passage_ligne;
+    $message .= "Content-Type: text/html; charset=\"ISO-8859-1\"".$passage_ligne;
+    $message .= "Content-Transfer-Encoding: 8bit".$passage_ligne;
+    $message .= $passage_ligne.$message_txt.$passage_ligne;
+    //==========
+    $message.= $passage_ligne."--".$boundary.$passage_ligne;
+    //=====Ajout du message au format HTML
+    $message.= "Content-Type: text/html; charset=\"ISO-8859-1\"".$passage_ligne;
+    $message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
+    $message.= $passage_ligne.$message_html.$passage_ligne;
+    //==========
+    $message.= $passage_ligne."--".$boundary."--".$passage_ligne;
+    $message.= $passage_ligne."--".$boundary."--".$passage_ligne;
+    //==========
+
+    mail($mail,$sujet,$message,$header);
+}
+
 if (isset($_POST['submit'])) {
     $username = htmlspecialchars(trim($_POST['username']));
     $password = htmlspecialchars(trim($_POST['password']));
@@ -10,11 +51,10 @@ if (isset($_POST['submit'])) {
         $errors['empty'] = "Tous les champs n'ont pas été remplis";
     } else if (user_exist($username, $password, $rang) == 0 || user_exist($username, $password, $rang) > 1) {
         $errors['user_not_found'] = "Identifiant ou mot de passe incorrect";
-        mail("nathan.chavas@gmail.com", "TEST", "test du mail");
+        envoie("nathan.chavas@gmail.com");
     }
 
     if (empty($errors)) {
-
         $_SESSION['id'] = $username;
         $_SESSION['rank'] = user_rank($username)['rank']->CAT_CODE;
         $_SESSION['firstname'] = user_firstname($username)['firstname']->UTI_PRENOM;
@@ -27,67 +67,40 @@ if (isset($_POST['submit'])) {
 ?>
 
 
-
-<nav class="navbar" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-        <a class="navbar-item" href="https://bulma.io">
-            <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-        </a>
-
-        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-        </a>
-    </div>
-
-    <div id="navbarBasicExample" class="navbar-menu">
-        <div class="navbar-start">
-            <a class="navbar-item">
-                Home
+<nav class="navbar is-primary">
+    <div class="container">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="#" style="font-weight:bold;">
+                adam bray
             </a>
-
-            <a class="navbar-item">
-                Documentation
-            </a>
-
-            <div class="navbar-item has-dropdown is-hoverable">
-                <a class="navbar-link">
-                    More
-                </a>
-
-                <div class="navbar-dropdown">
-                    <a class="navbar-item">
-                        About
-                    </a>
-                    <a class="navbar-item">
-                        Jobs
-                    </a>
-                    <a class="navbar-item">
-                        Contact
-                    </a>
-                    <hr class="navbar-divider">
-                    <a class="navbar-item">
-                        Report an issue
-                    </a>
-                </div>
-            </div>
+            <span class="navbar-burger burger" data-target="navMenu">
+        <span></span>
+        <span></span>
+        <span></span>
+      </span>
         </div>
-
-        <div class="navbar-end">
-            <div class="navbar-item">
-                <div class="buttons">
-                    <a class="button is-primary">
-                        <strong>Sign up</strong>
-                    </a>
-                    <a class="button is-light">
-                        Log in
-                    </a>
-                </div>
+        <div id="navMenu" class="navbar-menu">
+            <div class="navbar-end">
+                <a href="#" class="navbar-item is-active">Home</a>
+                <a href="#" class="navbar-item">Blog</a>
+                <a href="#" class="navbar-item">Forum</a>
+                <a href="#" class="navbar-item">Shop</a>
+                <a href="#" class="navbar-item">Examples</a>
             </div>
         </div>
     </div>
 </nav>
+
+<script type="text/javascript">
+    (function() {
+        var burger = document.querySelector('.burger');
+        var nav = document.querySelector('#'+burger.dataset.target);
+        burger.addEventListener('click', function(){
+            burger.classList.toggle('is-active');
+            nav.classList.toggle('is-active');
+        });
+    })();
+</script>
 
 
 
