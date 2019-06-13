2bab06aca07b4827eac5917fecb1d7310b7a8fc1
Esteban-psk <51739681+Esteban-psk@users.noreply.github.com>
2019-06-13 16:30:41 +0200

    Add files via upload

---
+++ www/js/ok.js
@@ -0,0 +1,84 @@
+var refs = {
+  Recap_semestre: {
+    open: function() { document.getElementById('Recap_semestre').classList.add('is-active');
+    },
+    close:function() { document.getElementById('Recap_semestre').classList.remove('is-active');
+                      
+    }
+  }
+  
+  
+};
+
+var refs1={
+	Recap_module: {
+    open: function() { document.getElementById('Recap_module').classList.add('is-active');
+    },
+    close:function() { document.getElementById('Recap_module').classList.remove('is-active');
+                      
+    }
+  }
+};
+
+var refs2={
+	Recap_enseignant: {
+    open: function() { document.getElementById('Recap_enseignant').classList.add('is-active');
+    },
+    close:function() { document.getElementById('Recap_enseignant').classList.remove('is-active');
+                      
+    }
+  }
+};
+
+var rtd ={
+	Modif_retard: {
+    open: function() { document.getElementById('Modif_retard').classList.add('is-active');
+    },
+    close:function() { document.getElementById('Modif_retard').classList.remove('is-active');
+                      
+    }
+  }
+};
+
+
+var abs ={
+	Modif_absence: {
+    open: function() { document.getElementById('Modif_absence').classList.add('is-active');
+    },
+    close:function() { document.getElementById('Modif_absence').classList.remove('is-active');
+                      
+    }
+  }
+};
+
+var m1 ={
+	m1_acc: {
+    open: function() { document.getElementById('m1_acc').classList.add('is-active');
+    },
+    close:function() { document.getElementById('m1_acc').classList.remove('is-active');
+                      
+    }
+  }
+};
+
+var michel = {
+  Consultation: {
+    open: function() { document.getElementById('Consultation').classList.add('is-active');
+    },
+    close:function() { document.getElementById('Consultation').classList.remove('is-active');
+               
+    }
+  }
+};
+
+var michel2 = {
+  information: {
+    open: function() { document.getElementById('information').classList.add('is-active');
+    },
+    close:function() { document.getElementById('information').classList.remove('is-active');
+               
+    }
+  }
+};
+
+
