0e36451e40f0e9bb65da1d75fb2d0db0e3694297
Sakura_Isana <sakura.troyard@gmail.com>
2019-06-14 10:55:13 +0200

    Update page retard

--- www/includes/director/delays.php
+++ www/includes/director/delays.php
@@ -3,106 +3,106 @@
         <article class="tile is-child">
             <article class="message">
                 <div class="message-header">
-                    <p>Tous vos billets d'absence</p>
+                    <p>Tous vos billets de retard</p>
                 </div>
                 <div class="message-body">
                     <table class="table is-fullwidth is-striped">
                         <thead>
-                        <tr>
-                            <th>Module</th>
-                            <th>Type</th>
-                            <th>État</th>
-                            <th>N°Étudiant</th>
-                            <th>Étudiant</th>
-                            <th>Enseignant</th>
-                            <th>Motif</th>
-                            <th>Commentaire</th>
-                            <th>Date</th>
-                            <th>Statut</th>
-                            <th>Options</th>
-                        </tr>
+                            <tr>
+                                <th>Module</th>
+                                <th>Type</th>
+                                <th>État</th>
+                                <th>N°Étudiant</th>
+                                <th>Étudiant</th>
+                                <th>Enseignant</th>
+                                <th>Motif</th>
+                                <th>Commentaire</th>
+                                <th>Date</th>
+                                <th>Statut</th>
+                                <th>Options</th>
+                            </tr>
                         </thead>
                         <tfoot>
                         </tfoot>
                         <tbody>
-                        <?php
-                        $color = null;
-                        $color_etat = null;
-                        $absences = list_director_delays();
-                        foreach($absences as $absence) {
-                            if($absence->SIG_TRAITE == 0) {
-                                $color = "is-danger";
-                            } else {
-                                $color = "is-success";
-                            }
-
-                            if($absence->SIG_ETAT == "Non justifié") {
-                                $color_etat = "is-danger";
-                            } else if ($absence->SIG_ETAT == "Rattrapage"){
-                                $color_etat = "is-warning";
-                            } else{
-                                $color_etat = "is-success";
-                            }
-
-                            ?>
-                            <tr>
-                                <td>
-                                    <strong><?php echo $absence->COU_MODULE; ?></strong>
-                                </td>
-                                <td>
-                                    <?php echo strtoupper($absence->COU_TYPE); ?>
-                                </td>
-                                <td>
-                                    <span class="tag <?php echo $color_etat ?> is-rounded"><?php echo $absence->SIG_ETAT; ?></span>
-                                </td>
-                                <td>
-                                    <?php echo $absence->UTI_IDENTIFIANT; ?>
-                                </td>
-                                <td>
-                                    <span class="uppercase">
-                                        <?php echo strtoupper($absence->UTI_NOM); ?>
-                                    </span>
-                                    <?php echo $absence->UTI_PRENOM; ?>
-                                </td>
-                                <td>
-                                    <span class="uppercase">
-                                        <?php echo strtoupper($absence->UTI_ENS_NOM); ?>
-                                    </span>
-                                    <?php echo $absence->UTI_ENS_PRE; ?>
-                                </td>
-                                <td>
-                                    <?php echo $absence->SIG_MOTIF; ?>
-                                </td>
-                                <td>
-                                    <?php echo $absence->SIG_COMMENTAIRE; ?>
-                                </td>
-                                <td>
-                                    <?php echo $absence->SIG_DATE; ?>
-                                </td>
-                                <td>
-                                    <label class="checkbox">
-                                        <input type="checkbox">
-                                    </label>
-                                </td>
-                                <td>
-                                    <form method="POST">
-                                        <input type="hidden" value="<?php echo $retard->SIG_CODE; ?>" id="code" name="code">
-                                        <button type="submit" href="#" class="button is-info" aria-haspopup="true" onclick="abs.Modif_retard.open()">
-                                            <span class="icon is-small">
-                                                <i class="far fa-edit"></i>
-                                            </span>
-                                        </button>
-                                        <button type="submit" class="button is-danger" id="delete" name="delete">
-                                            <span class="icon is-small">
-                                                <i class="fas fa-trash-alt"></i>
-                                            </span>
-                                        </button>
-                                    </form>
-                                </td>
-                            </tr>
                             <?php
-                        }
-                        ?>
+                            $color = null;
+                            $color_etat = null;
+                            $retards = list_director_delays();
+                            foreach($retards as $retard) {
+                                if($retard->SIG_TRAITE == 0) {
+                                    $color = "is-danger";
+                                } else {
+                                    $color = "is-success";
+                                }
+
+                                if($retard->SIG_ETAT == "Non justifié") {
+                                    $color_etat = "is-danger";
+                                } else if ($retard->SIG_ETAT == "Rattrapage"){
+                                    $color_etat = "is-warning";
+                                } else{
+                                    $color_etat = "is-success";
+                                }
+
+                                ?>
+                                <tr>
+                                    <td>
+                                        <strong><?php echo $retard->COU_MODULE; ?></strong>
+                                    </td>
+                                    <td>
+                                        <?php echo strtoupper($retard->COU_TYPE); ?>
+                                    </td>
+                                    <td>
+                                        <span class="tag <?php echo $color_etat ?> is-rounded"><?php echo $retard->SIG_ETAT; ?></span>
+                                    </td>
+                                    <td>
+                                        <?php echo $retard->UTI_IDENTIFIANT; ?>
+                                    </td>
+                                    <td>
+                                        <span class="uppercase">
+                                            <?php echo $retard->UTI_NOM; ?>
+                                        </span>
+                                        <?php echo $retard->UTI_PRENOM; ?>
+                                    </td>
+                                    <td>
+                                        <span class="uppercase">
+                                            <?php echo $retard->UTI_ENS_NOM; ?>
+                                        </span>
+                                        <?php echo $retard->UTI_ENS_PRE; ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $retard->SIG_MOTIF; ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $retard->SIG_COMMENTAIRE; ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $retard->SIG_DATE; ?>
+                                    </td>
+                                    <td>
+                                        <label class="checkbox">
+                                            <input type="checkbox">
+                                        </label>
+                                    </td>
+                                    <td>
+                                        <form method="POST">
+                                            <input type="hidden" value="<?php echo $retard->SIG_CODE; ?>" id="code" name="code">
+                                            <button type="submit" href="#" class="button is-info" aria-haspopup="true" onclick="abs.Modif_retard.open()">
+                                                <span class="icon is-small">
+                                                    <i class="far fa-edit"></i>
+                                                </span>
+                                            </button>
+                                            <button type="submit" class="button is-danger" id="delete" name="delete">
+                                                <span class="icon is-small">
+                                                    <i class="fas fa-trash-alt"></i>
+                                                </span>
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                                <?php
+                                }
+                            ?>
                         </tbody>
                     </table>
                 </div>
@@ -110,18 +110,18 @@
         </article>
     </div>
 </div>
-<div id="Modif_retard" class="modal">
-    <div class="modal-background" onclick="rtd.Modif_retard.close()"></div>
+<div class="modal" id="Modif_absence">
+    <div class="modal-background" onclick="abs.Modif_absence.close()"></div>
     <div class="modal-card">
         <header class="modal-card-head">
             <p class="modal-card-title">
                 Nom Prénom | Promo - Groupe
             </p>
-            <button class="delete" aria-label="close" onclick="rtd.Modif_retard.close()"></button>
+            <button class="delete" aria-label="close" onclick="abs.Modif_absence.close()"></button>
         </header>
         <section class="modal-card-body">
             <strong>N°Étudiant : </strong><span class="tag is-primary is-rounded" style="margin-right: 10px;">21800346</span>
-            <br><strong>Type : </strong><span class="tag is-warning is-rounded" style="margin-right: 10px;">Retard</span>
+            <br><strong>Type : </strong><span class="tag is-warning is-rounded" style="margin-right: 10px;">Absence</span>
             <br><strong>Motif : </strong><textarea class="textarea has-fixed-size" placeholder="Motif" rows="1"></textarea>
             <br><strong>État : </strong><span class="tag" style="margin-right: 10px;">
                 <div class="select is-danger is-rounded">
