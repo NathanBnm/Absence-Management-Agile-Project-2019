c569512b24f2b2ad4da15a592c5d8f63509c20de
Jessy Monnier <21800847@campus.unicaen.fr>
2019-06-14 11:24:59 +0200

    Modification du footer3

--- www/includes/body/footer.php
+++ www/includes/body/footer.php
@@ -2,9 +2,8 @@
 <footer class="footer">
   <div class="content has-text-centered">
     <p>
-      <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
-      <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
-      is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+      COPYRIGHT ©2018 · PROJET AGILE · TOUS DROITS RÉSERVÉS -
+	  APPLICATION CRÉÉ PAR JESSY, BAPTISTE, NATHAN B., NATHAN C., CLEMENCE, GERMAIN, YOUMNA, ESTEBAN - MENTIONS LÉGALES  - POLITIQUE DE CONFIDENTIALITÉ
     </p>
   </div>
 </footer>
