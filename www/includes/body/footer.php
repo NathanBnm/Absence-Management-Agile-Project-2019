3955c33fde96f08f1513de5667613e226da5e4a1
Jessy Monnier <21800847@campus.unicaen.fr>
2019-06-14 11:14:48 +0200

    Modification du footer

--- www/includes/body/footer.php
+++ www/includes/body/footer.php
@@ -1,12 +1,15 @@
 </body>
-<footer class="footer is-fixed-bottom has-background-grey-dark has-text-white" style="margin-top: 100px;">
-    <div class="columns">
-        <div class="column has-text-centered is-2 is-italic">
-            2019&copy; Equipe A3
-        </div>
-        <div class="column has-text-centered ">
-            Projet agile : Gestion des absences
-        </div>
-    </div>
+<footer>
+	<div style="float: left;><font size="2">
+		<p> GROUPE A3 </p></font>
+		<a href="https://www.facebook.com" title="Suivez-nous sur facebook !">
+		<img src="https://www.calliframe.com/wp-content/uploads/2013/12/facebook.png" /></a>
+		<span style="font-family: 'Century Gothic', arial"><font size="0">
+	</div>
+	<div style="float: center;><font size="2">
+			<p>COPYRIGHT ©2018 · PROJET AGILE ABSENCE · TOUS DROITS RÉSERVÉS -</p>
+			<p>SITE CRÉÉ PAR JESSY , BAPTISTE, CLEMENCE, THOMAS, GERMAIN, NATHAN C. ET NATHAN B. - MENTIONS LÉGALES  - POLITIQUE DE CONFIDENTIALITÉ</p>
+		</font></span>
+	</div>
 </footer>
 </html>
