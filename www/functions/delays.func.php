1775ecfef4ebc5ea494cbb1254f901cf23d7a873
Sakura_Isana <sakura.troyard@gmail.com>
2019-06-14 10:46:38 +0200

    Test affichage nom profs

--- www/functions/delays.func.php
+++ www/functions/delays.func.php
@@ -54,12 +54,16 @@ function list_director_delays()
     $u = [
         'UTI_IDENTIFIANT' => $_SESSION['id']
     ];
-    $sql = "SELECT absence.SIG_COMMENTAIRE, absence.SIG_MOTIF, absence.SIG_ETAT, etu.UTI_PRENOM, etu.UTI_NOM, etu.UTI_IDENTIFIANT, cours.COU_MODULE, DATE_FORMAT(SIG_DATE, 'Le %d/%m/%Y à %H:%i') AS SIG_DATE, SIG_TRAITE, COU_TYPE, SIG_ETAT, SIG_CODE
-            FROM ABS_BILLET absence
-            JOIN ABS_COURS cours ON absence.COU_CODE = cours.COU_CODE
-            JOIN ABS_UTILISATEUR etu ON etu.UTI_CODE = absence.UTI_CODE_1
-            WHERE absence.SIG_TYPE = 'R'
-            ORDER BY SIG_CODE DESC";
+    $sql = "SELECT absence.SIG_COMMENTAIRE, absence.SIG_MOTIF, absence.SIG_ETAT, etu.UTI_PRENOM, etu.UTI_NOM, etu.UTI_IDENTIFIANT, 
+    ens.UTI_NOM as 'UTI_ENS_NOM', ens.UTI_PRENOM as 'UTI_ENS_PRE', cours.COU_MODULE, 
+    DATE_FORMAT(SIG_DATE, 'Le %d/%m/%Y à %H:%i') AS SIG_DATE, SIG_TRAITE, COU_TYPE, SIG_ETAT, SIG_CODE 
+    FROM ABS_BILLET absence 
+    JOIN ABS_COURS cours ON absence.COU_CODE = cours.COU_CODE 
+    JOIN ABS_UTILISATEUR etu ON etu.UTI_CODE = absence.UTI_CODE_1 
+    JOIN ABS_UTILISATEUR ens ON ens.UTI_CODE = absence.UTI_CODE 
+    WHERE absence.SIG_TYPE = 'A' 
+    AND absence.UTI_CODE = (SELECT UTI_CODE FROM ABS_UTILISATEUR WHERE UTI_IDENTIFIANT = :UTI_IDENTIFIANT) 
+    ORDER BY SIG_CODE DESC ";
     $req = $db->prepare($sql);
     $req->execute($u);
     $delays = [];
